7aae21776fef4a75ec073934192184e9c12072a5
Katrascia <ar4311@tc.columbia.edu>
2025-11-16 23:47:39 -0500

    connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from 'cors'
 
+import "dotenv/config"
+import connectDB from "./db.js";
+
 const app = express()
 const port = 3000
 
@@ -12,4 +15,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log('Listening on port:' + port);
+    connectDB()
 })
