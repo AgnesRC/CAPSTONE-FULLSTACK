0cf199d0aa650a96b615830d6cc8709ac8e0a7bb
Katrascia <ar4311@tc.columbia.edu>
2025-11-21 13:54:47 -0500

    routes added

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -18,4 +18,28 @@ router.get("/", async (req, res) => {
     res.json(projects);
   });
 
+router.put("/:id", async (req, res) => {
+    try {
+      const updated = await Project.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      });
+      res.json(updated);
+    } catch (error) {
+      res.json(error.message);
+    }
+  });
+  
+router.delete("/:id", async (req, res) => {
+    try {
+      const deleted = await Project.findByIdAndDelete(req.params.id);
+      res.json(deleted);
+    } catch (error) {
+      res.json(error.message);
+    }
+  });
+
+
+
+
+
   export default router;
